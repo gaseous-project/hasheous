c72127823263b18966d10fae2e5acab5b918e0e8
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2024-09-22 06:21:03 -0700

    Added a lookup for Platforms (#61)

--- hasheous/Controllers/V1.0/LookupController.cs
+++ hasheous/Controllers/V1.0/LookupController.cs
@@ -1,4 +1,5 @@
 using Classes;
+using hasheous_server.Classes;
 using hasheous_server.Models;
 using Microsoft.AspNetCore.Mvc;
 using static Authentication.ApiKey;
@@ -84,6 +85,52 @@ namespace hasheous_server.Controllers.v1_0
             catch (HashLookup.HashNotFoundException hnfEx)
             {
                 return NotFound("The provided hash was not found in the signature database.");
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Get a list of all available platforms in the database
+        /// </summary>
+        /// <returns>A list of all available platforms in the database</returns>
+        /// <response code="200">Returns a list of all available platforms in the database</response>
+        /// <response code="404">If no platforms are found in the database</response>
+        /// <response code="400">If the page number or page size values are invalid</response>
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ResponseCache(CacheProfileName = "7Days")]
+        [Route("Platforms")]
+        public async Task<IActionResult> Platforms(int PageNumber = 1, int PageSize = 100)
+        {
+            if (PageSize > 1000)
+            {
+                return BadRequest("PageSize must be less than or equal to 1000.");
+            }
+
+            if (PageNumber < 1)
+            {
+                return BadRequest("PageNumber must be greater than or equal to 1.");
+            }
+
+            try
+            {
+                DataObjects dataObjects = new DataObjects();
+                DataObjectsList platforms = dataObjects.GetDataObjects(DataObjects.DataObjectType.Platform, PageNumber, PageSize);
+
+                if (platforms.Count == 0)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(platforms);
+                }
             }
             catch
             {
