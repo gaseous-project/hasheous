67b7e2190a5bc7e4ad53b1e93cb151bcd6e6b404
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2026-07-17 23:17:40 +1000

    feat: Implement full-text search enhancements
    - Added a new method `BuildFullTextBooleanPrefixQuery` in `Common.cs` to construct boolean prefix queries for full-text search.
    - Updated SQL queries in `DataObjects.cs` to utilize full-text search with the new method for improved search capabilities.
    - Modified `SignatureManagement.cs` to use the new full-text query method for name matching in boolean mode.
    - Added a new full-text index for `DataObject` in the SQL schema to support the updated search functionality.

--- hasheous-lib/Schema/hasheous-1037.sql
+++ hasheous-lib/Schema/hasheous-1037.sql
@@ -3,3 +3,5 @@ CREATE FULLTEXT INDEX idx_publisher_ft ON Signatures_Publishers (`Publisher`);
 CREATE FULLTEXT INDEX idx_platform_ft ON Signatures_Platforms (`Platform`);
 
 CREATE FULLTEXT INDEX idx_rom_ft ON Signatures_Roms (`Name`);
+
+CREATE FULLTEXT INDEX idx_name_ft ON DataObject (`Name`);
